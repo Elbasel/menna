d0605c0b13647e43fe3753e56e7429c1440fd033
elbasel <abdelrahman.elbasel@gmail.com>
2021-12-19 07:31:09 +0200

    added logic

--- script.js
+++ script.js
@@ -6,35 +6,71 @@ function getRandomArbitrary(min, max) {
 
 function changeText() {
 
+    if (fashel.currentTime < fashel.duration && counter > 7) {
+        return
+    }
+
+    if (counter > 12 && counter < 20) {
+        debugger
+        catVideo = document.querySelector('video')
+        if (!!catVideo) {
+            if (catVideo.currentTime < 40) return
+        }
+    }
+
     if (counter === 1) {
         music.play()
-    } else if (counter === 6) {
+    } else if (counter === 7) {
         const pauseTime = music.currentTime
         music.pause()
+        fashel.addEventListener('timeupdate', updateProg)
         fashel.play()
+        addProg()
         fashel.onended = function() {
             music.currentTime = pauseTime + 10
             music.play()
         }
+        counter++
+        return
 
-    } else if ([11, 12, 13, 14].includes(counter)) {
+        // } else if ([12, 13, 14].includes(counter)) {
 
+    } else if (counter === 12) {
         // debugger
+        music.pause()
         body.innerHTML = ''
         const catVideo = document.createElement('video')
         catVideo.setAttribute('src', './cat.mp4')
-        catVideo.setAttribute('controls', 'controls')
-        body.appendChild(catVideo)
+            // catVideo.setAttribute('controls', 'controls')
         catVideo.play()
         counter++
+        // catVideo.addEventListener('click', () => counter++)
+        // catVideo.addEventListener('tap', () => counter++)
+        // catVideo.addEventListener('touchstart', () => counter++)
+        catVideo.onclick = () => counter++;
+        catVideo.setAttribute('onclick', () => counter++)
+            // body.appendChild(catVideo)
+        const container = document.createElement('div')
+        container.addEventListener('click', () => counter++)
+        body.appendChild(container)
+        container.appendChild(catVideo)
+
         return
 
     }
 
 
+    if (counter > 12 && counterFlag) {
+        counter = 12
+        counterFlag = false
+    }
+
     body.innerHTML = ''
 
     const newHead = document.createElement('h1')
+    if (counter > textList.length) {
+        counter = textList.length - 1
+    }
     newHead.textContent = textList[counter]
 
     const randomAnimationIndex = getRandomArbitrary(0, animationList.length)
@@ -47,13 +83,28 @@ function changeText() {
 }
 
 
+function addProg() {
+    const prog = document.createElement('progress')
+    prog.setAttribute('max', 100)
+    prog.setAttribute('value', 0)
+    body.appendChild(prog)
+
+    updateProg()
+}
+
+function updateProg() {
+    const prog = document.querySelector('progress')
+    const percentage = Math.floor(100 / fashel.duration) * fashel.currentTime
+    prog.value = percentage + 2
+}
 
 animationList = ['focus-in-expand', 'tracking-in-contract-bck-bottom', 'tracking-in-contract', 'animate__bounceIn', 'animate__flip']
 
 fontStylesList = ['flaming-text', 'blue-background-text', 'lux', 'neon', 'comic']
 
+let counterFlag = true
 
-let textList = ['MENNA', 'Hello', 'mesh betzakry leeeh??', 'mama betkolk zakry ya fashla', 'we kefaya LUDO', 'we kol showa neshofk online 3la facebook', 'shofy halek ya fashla', 'yla komy zakry', 'mesh kolt komy zakry!!!', 'seby el mobile we komy zakry yla', 'enty lesa hena???', 'shofy el video dah we komy zakry yla', 'yla 5las keda mfesh 7aga tany']
+let textList = ['MENNA', 'Hello', 'mesh betzakry leeeh??', 'mama betkolk zakry ya fashla', 'we kefaya LUDO', 'we kol showa neshofk online 3la facebook', 'shofy halek ya fashla', 'yla komy zakry', 'mesh kolt komy zakry!!!', 'seby el mobile we komy zakry yla', 'enty lesa hena???', 'shofy el video dah we komy zakry yla', 'yla 5las keda mfesh 7aga tany', '5las ya benty mfesh 7aga tany kolna', 'ro7y 5las', 'bye bye', 'slamoz', 'go to hell!', 'bs b2a', 'don\'t click anymore please!', 'byeeeeeeeeeeeeeee']
 let counter = 0;
 
 const head = document.querySelector('h1')
